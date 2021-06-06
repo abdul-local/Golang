c1aeedc12baa3d5573e99328f5da536b8df7dbe2
abdul-local <abdullah.hamzan@gmail.com>
2021-06-07 03:01:46 +0800

    learn golang

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// fungsi untuk menampilkan hello word
+	fmt.Println("hello word")
+
+	// deklarasikan sebuah variabel
+	var firstname string = "abdul hamzan"
+	var lastname string
+	lastname = "wadi"
+	fmt.Printf("halo %s %s !\n", firstname, lastname)
+	var firstname1 = "abdul"
+	// tanpa mengunakan var, tanpa tipe data
+	lastname1 := "hamzan"
+
+	fmt.Printf("halo %s %s !\n", firstname1, lastname1)
+
+	// untuk multi variabel
+
+	var one, two, three string
+	one, two, three = "satu", "dua", "tiga"
+	fmt.Printf("urutan %s %s %s", one, two, three)
+
+	// variabel underScore
+	_ = "belajar golang"
+	name, _ := "hamazam", "erik"
+	fmt.Printf(" halow %s \n", name)
+
+	// deklarasikan variable dengan keyword new untuk pointer
+
+	name1 := new(string)
+
+	fmt.Println(name1)
+	fmt.Println(*name1)
+
+}
